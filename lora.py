349534b591f99e9ee96838eb615b53e8d01a6dc6
Riccardo Valdo <valdoriccardo@gmail.com>
2026-03-01 16:51:15 +0100

    lora inject implementation

--- lora.py
+++ lora.py
@@ -1,14 +1,24 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class LoRAConfig:
+    rank: int = 4
+    alpha: int = 16
+    dropout: float = 0.05
+    #layer injections (default factory creates a list every time the class is initalized on different mem positions)
+    target_layers: List[str] = field(default_factory= lambda: ["c_attn", "c_proj", "c_fc"])
 
 class LoRA(nn.Module):
-    def __init__(self, original_layer: nn.Linear, rank: int = 4, alpha: int = 16):
+    def __init__(self, original_layer: nn.Linear, config: LoRAConfig = LoRAConfig()):
         super().__init__()
 
         #save and freeze the original layer
         self.original_layer = original_layer
-        
+
         self.original_layer.weight.requires_grad = False
         if self.original_layer.bias is not None:
             self.original_layer.bias.requires_grad = False
@@ -17,13 +27,14 @@ class LoRA(nn.Module):
         self.out_features = original_layer.out_features
 
         #lora params
-        self.rank = rank
-        self.alpha = alpha
+        self.rank = config.rank
+        self.alpha = config.alpha
         self.scaling = self.alpha / self.rank
 
         #lora layer
         self.lora_b = nn.Linear(self.in_features, self.rank, bias = False)
         self.lora_a = nn.Linear(self.rank, self.out_features, bias = False)
+        self.dropout = nn.Dropout(p= config.dropout)
 
         #initialiaze only lora A and B
         self.reset_params()
@@ -37,8 +48,34 @@ class LoRA(nn.Module):
         nn.init.zeros_(self.lora_b.weight)
     
     def forward(self, x):
+        w_x = self.original_layer(x)
+        a_x = self.lora_a(self.dropout(x))
+        b_x = self.lora_b(a_x)
+        y = w_x + self.lora_b(a_x) * self.scaling
+        return y
 
-    
+    @classmethod
+    def inject_lora(cls, model, config: LoRAConfig):
+        """
+        Take a GPT class model as an arg and inject lora layers in place of any linear layer but lm_head 
+        """
+
+        for mn, m in model.named_modules():
+            for cn, c in m.named_children():
+                
+                if isinstance(c, nn.Linear) and any(target in cn for target in config.target_layers):
+
+                    lora_layer = cls(c)
+                    setattr(m, cn, lora_layer)
+        
+        return model
+        
+
+
+
+
+        
+
 
 
 
--- verify.py
+++ verify.py
@@ -4,16 +4,12 @@ from model import GPT
 
 model = GPT.from_pretrained()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
 model.to(device)
 model.eval()
 
 enc = tiktoken.get_encoding('gpt2')
-txt = """Q: What is the capital of Germany?
-A: Berlin
-Q: What is the capital of Italy?
-A: Rome
-Q: What is the capital of France?
-"""    
+txt = """What's the capital of France?"""    
 tokens = torch.tensor(enc.encode(txt), dtype=torch.long).unsqueeze(0).to(device)
 
 with torch.no_grad():
@@ -21,7 +17,8 @@ with torch.no_grad():
                               max_new_tokens = 20,
                               do_sample= True,
                               top_k = 10,
-                              temperature = 1.0)
+                              temperature = 1.0,
+                              stop_on_nl = True)
 
 response = enc.decode(complete[0].tolist())
 print(response)

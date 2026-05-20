b998ef617ac5e5b1a4668d71270ec05bd3800691
Riccardo Valdo <valdoriccardo@gmail.com>
2026-05-20 20:03:49 +0200

    basic functionality implemented and tested

--- inference.py
+++ inference.py
@@ -1,70 +1,93 @@
-import argparse
+import os 
+import tiktoken
 import torch
-import os
-from datetime import datetime
-
-from data import get_dataloaders
-from model import GPT, GPTConfig
-from lora import LoRA, LoRAConfig
-from trainer import train_model, evaluate_loss
+import argparse
+import re
+from model import GPT
+from pathlib import Path
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Train a local version of GPT2")
+    parser = argparse.ArgumentParser(description = "Start a chat with a finetuned model of GPT2 or the pretrained one.")
 
-    #general args
-    parser.add_argument("--batch_size", type=int, default=4, help="Batch size per step")
-    parser.add_argument("--block_size", type=int, default=128, help="Context window size")
-    parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate")
-    parser.add_argument("--epochs", type=int, default=3, help="Number of full passes over data")
-
-    #lora args
-    parser.add_argument("--use_lora", action="store_true", help="Add this flag to enable Low Rank Adaptation")
-    parser.add_argument("--lora_rank", type=int, default=4, help="Rank for LoRA matrices")
-    parser.add_argument("--lora_alpha", type=int, default=16, help="Alpha scaling parameter for LoRA")
-    parser.add_argument("--lora_dropout", type=float,  )
+    #general inference arguments
+    parser.add_argument("--ft", action = "store_true", help = "Use the latest finetuned model if present.")
+    parser.add_argument("--temperature", type = float, default = 1.0, help = "Temperature used to generate the next token.")
 
     return parser.parse_args()
+
+def terminal_chat(model, device = "cuda", temperature = 1.0):
+    model.to(device)
+    enc = tiktoken.get_encoding("gpt2")
+    print("="*50)
+    print("Welcome to your GPT2 LoRA finetuned model - (type 'exit' or 'q' to exit)")
+    print("="*50)
+
+    while True:
+        input_txt = input("\nYou: ")
+
+        if input_txt.lower() in ['exit', 'q']:
+            print("Goodbye")
+            break
+        if not input_txt.strip():
+            continue
+
+        prompt = torch.tensor(enc.encode(input_txt), dtype = torch.long).unsqueeze(0).to(device)
+        amp_dtype = torch.float16 if device == "cuda" else torch.bfloat16
+
+        with torch.no_grad():
+            # with torch.amp.autocast(device_type = device, dtype = amp_dtype):
+                generated_text = ""
+                print("\nAssistant: ")
+                for t in model.generate(idx = prompt, max_new_tokens = 100, temperature = temperature):
+                    word = enc.decode([t])
+                    generated_text += word
+                    print(word, end = "", flush = True)
+
+                    if any(generated_text.endswith(seq) for seq in ["\n"]):
+                        break
+
+def get_latest(dir: str, prefix="model_ft_"):
+    """
+    Scans the specified directory and returns the Path to the most recent 
+    model file matching the 'prefix_YYYYMMDD_HHMMSS' format.
+    
+    Returns None if no matching files are found.
+    """
+    
+    directory = Path(dir)
+    timestamp_pattern = re.compile(rf"^{prefix}\d{{8}}_\d{{6}}")
+    matching_files = []
+
+    for file in directory.iterdir():
+        if file.is_file() and timestamp_pattern.match(file.name):
+            matching_files.append(file)
+    if not matching_files:
+        return None
+    
+    latest_model = max(matching_files, key = lambda f: f.name)
+    return latest_model
 
 
 def main():
     args = parse_args()
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print(f"Running on {device}")
-    
-    train, val = get_dataloaders(args.batch_size, args.block_size)
 
-    print("Initializing the model...")
-    pretrained_model = GPT.from_pretrained()
-
-    if args.use_lora:
-        print("Injecting LoRA modules inside the model")
-        model = LoRA.inject_lora(pretrained_model, lora_config= LoRAConfig(
-            rank = args.lora_rank,
-            alpha = args.lora_alpha,
-            dropout= args.lora_dropout
-            ))
-        trainable_params = [p for p in model.parameters() if p.requires_grad]
-        optimizer = torch.optim.AdamW(trainable_params, 
-                                      lr = 3e-4, 
-                                      weight_decay = 0.01) #optimizer defaul when using lora
+    if args.ft:
+        latest_model = get_latest("./models/")
+        if not latest_model:
+            print("A finetuned model wasn't found so the pretrained will be used.")
+            model = GPT.from_pretrained()
+        else:
+            pass
+            #implement the logic to fuse lora model
     else:
-        print("Training the entire model without LoRA")
-        model = pretrained_model 
-        optimizer = GPT.configure_optimizer() #optimizer w/ weight decay when performing classic ft
+        model = GPT.from_pretrained()
     
-    model = model.to(device)
 
-    #training loop 
-    ft_model = train_model(model = model,
-                           train_loader = train,
-                           val_loader = val,
-                           optimizer = optimizer ,
-                           epochs = args.epochs,
-                           device = device)
-    if not os.path.exists("./models/"):
-        os.mkdir("./models")
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    torch.save(ft_model,f"./models/model_ft_{timestamp}")
+    device  = "cuda" if torch.cuda.is_available() else "cpu"
+    terminal_chat(model = model, device = device, temperature = args.temperature)
+            
+
 
 if __name__ == "__main__":
     main()
+

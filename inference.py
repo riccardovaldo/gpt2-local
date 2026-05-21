c48cc5c7afe9e4624f7084d9aa3ed9aa3fbe77f0
Riccardo Valdo <valdoriccardo@gmail.com>
2026-05-21 19:48:27 +0200

    inference now load checkpoints and fixed the eval inside the terminal chat which was using the model in training mode

--- inference.py
+++ inference.py
@@ -2,21 +2,21 @@ import os
 import tiktoken
 import torch
 import argparse
-import re
 from model import GPT
+from lora import LoRA
 from pathlib import Path
 
 def parse_args():
     parser = argparse.ArgumentParser(description = "Start a chat with a finetuned model of GPT2 or the pretrained one.")
 
     #general inference arguments
-    parser.add_argument("--ft", action = "store_true", help = "Use the latest finetuned model if present.")
+    parser.add_argument("--checkpoint", type = str, default = None, help = "Provide the path to a checkpoint file for either full ft or lora adapters .pt")
     parser.add_argument("--temperature", type = float, default = 1.0, help = "Temperature used to generate the next token.")
 
     return parser.parse_args()
 
 def terminal_chat(model, device = "cuda", temperature = 1.0):
-    model.to(device)
+    model.to(device).eval()
     enc = tiktoken.get_encoding("gpt2")
     print("="*50)
     print("Welcome to your GPT2 LoRA finetuned model - (type 'exit' or 'q' to exit)")
@@ -34,56 +34,62 @@ def terminal_chat(model, device = "cuda", temperature = 1.0):
         prompt = torch.tensor(enc.encode(input_txt), dtype = torch.long).unsqueeze(0).to(device)
         amp_dtype = torch.float16 if device == "cuda" else torch.bfloat16
 
+        generated_tokens = []
+        printed_text = []
+
         with torch.no_grad():
-            # with torch.amp.autocast(device_type = device, dtype = amp_dtype):
-                generated_text = ""
+            with torch.amp.autocast(device_type = device, dtype = amp_dtype):
                 print("\nAssistant: ")
                 for t in model.generate(idx = prompt, max_new_tokens = 100, temperature = temperature):
-                    word = enc.decode([t])
-                    generated_text += word
-                    print(word, end = "", flush = True)
 
-                    if any(generated_text.endswith(seq) for seq in ["\n"]):
+                    generated_tokens.append(t)
+                    current_text = enc.decode(generated_tokens)
+                    #extracting the new generated words that havent been printed
+                    new_chars = current_text[len(printed_text):]
+                    print(new_chars, end = "", flush = True)
+                    printed_text = current_text
+
+                    if "\n" in new_chars:
                         break
+            print()
 
-def get_latest(dir: str, prefix="model_ft_"):
-    """
-    Scans the specified directory and returns the Path to the most recent 
-    model file matching the 'prefix_YYYYMMDD_HHMMSS' format.
-    
-    Returns None if no matching files are found.
-    """
-    
-    directory = Path(dir)
-    timestamp_pattern = re.compile(rf"^{prefix}\d{{8}}_\d{{6}}")
-    matching_files = []
 
-    for file in directory.iterdir():
-        if file.is_file() and timestamp_pattern.match(file.name):
-            matching_files.append(file)
-    if not matching_files:
-        return None
-    
-    latest_model = max(matching_files, key = lambda f: f.name)
-    return latest_model
 
 
 def main():
-    args = parse_args()
-
-    if args.ft:
-        latest_model = get_latest("./models/")
-        if not latest_model:
-            print("A finetuned model wasn't found so the pretrained will be used.")
-            model = GPT.from_pretrained()
-        else:
-            pass
-            #implement the logic to fuse lora model
-    else:
-        model = GPT.from_pretrained()
-    
-
     device  = "cuda" if torch.cuda.is_available() else "cpu"
+    args = parse_args()
+    model = GPT.from_pretrained()
+    
+    if args.checkpoint is None:
+        print("No checkpoint provided. Running vanilla pretrained model.")
+        
+    else:
+        ckpt_path = Path(args.checkpoint)
+        if not ckpt_path.exists():
+            raise FileNotFoundError(f"No checkpoint found at {ckpt_path}")
+        else:
+            try:
+                print(f"Loading checkpoint from {ckpt_path}...")
+                checkpoint = torch.load(ckpt_path, map_location = device)
+                state_dict = checkpoint.get("model_state_dict", checkpoint)
+                is_lora = any("lora_"  in k for k in state_dict.keys())
+
+                if is_lora:
+                    print("Detected Lora checkpoint...")
+                    model_lora = LoRA.inject_lora(model)
+                    model_lora.load_state_dict(state_dict, strict = False)
+                    model_lora.merge_and_unload()
+                    print("Lora adapters loaded and weights merged.")
+                    model = model_lora
+                else:
+                    print("Loading the finetuned weights of the model.")
+                    model.load_state_dict(state_dict, strict = False)
+            except:
+                raise KeyError("Error in retrieving and laoding the weights.")
+
+
+    
     terminal_chat(model = model, device = device, temperature = args.temperature)
             
 
